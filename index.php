4e8e98d1acd7e340ae86acfc635b64c597bbd9b2
Binyi Han <binyihan@Binyis-MacBook-Pro.local>
2017-11-03 22:14:00 -0700

    init

---
+++ index.php
@@ -0,0 +1,110 @@
+<html>
+
+
+	<head>
+		
+		
+		<title>while eating</title>
+		
+		<link href="https://fonts.googleapis.com/css?family=Architects+Daughter" rel="stylesheet">
+		
+		<style type="text/css">
+		
+			a	{
+			
+				color: black;
+				text-decoration: none;
+			
+            }
+
+            a:hover {
+
+				color: green;
+                text-decoration: none;
+                font-size: 200%;
+
+            }
+		
+			.first-section {
+			
+				background-color:#4285F4;
+				float:left;
+				width:350px;
+				position:relative;
+				opacity:0.8;
+				border-radius: 10px;
+				font-family: 'Architects Daughter', cursive, KaiTi;
+				font-size: 120%;
+			
+			}
+		
+			.second-section {
+			
+				float:right;
+		
+			
+			}
+		
+		
+		</style>
+	
+	
+	
+	</head>
+
+
+
+
+	<body>
+	
+		<div class="first-section">
+		
+			<h1>video</h1>
+			
+				<ul>
+					
+					<li>game</li>
+                        <a href="http://space.bilibili.com/1565155/#!/">主播真会玩</a><br>
+						<a href="http://space.bilibili.com/10462362/#!/">逗鱼时刻</a><br>
+						<a href="http://space.bilibili.com/20990353/#!/">努巴尼欢乐秀</a><br>
+					
+
+					<li>animation</li>
+						<a href="http://bangumi.bilibili.com/anime/5978/">傅人传</a><br>
+						<a href="http://www.dnvod.tv/Movie/detail.aspx?id=2aGLezX2DZk%3d">进击的巨人</a><br>
+						
+					<li>funny</li>
+						<a href="http://www.bilibili.com/video/life.html">好玩</a><br>
+
+						
+					<li>quick movie review</li>
+						<a href="http://space.bilibili.com/927587/#!/">木鱼</a><br>
+						
+					<li>bao zou da shi jian</li>
+					
+						<a href="http://space.bilibili.com/883968/#!/">暴走大事件</a><br>
+						
+					<li>series</li>
+						<a href="http://www.dnvod.tv/Movie/detail.aspx?id=BhdIRzJebHQ%3d">人民的名义</a><br>
+						
+					<li>guita</li>
+					
+					<li>asmr</li>
+						<a href="https://www.youtube.com/watch?v=q2PgiV9_K0s">轩子巨二兔1</a><br>
+						<a href="https://www.youtube.com/watch?v=gQJ6fqQWgns">轩子巨二兔2</a><br>
+				</ul>
+			
+		</div>
+		
+		<div class="second-section">
+		
+			<iframe width="560" height="315" src="https://www.youtube.com/embed/0QjF6A3Zwkk" frameborder="0" allowfullscreen></iframe><br>
+		
+		</div>
+	
+	</body>
+
+
+
+
+</html>
