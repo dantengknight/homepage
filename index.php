e6ec674fcef1b82b67b0dfdd15e454bc0db4e0f8
Binyi Han <binyihan@Binyis-MacBook-Pro.local>
2017-11-04 22:14:22 -0700

    js, rats game init

--- index.php
+++ index.php
@@ -9,12 +9,25 @@
 		<link href="https://fonts.googleapis.com/css?family=Architects+Daughter" rel="stylesheet">
 		
 		<style type="text/css">
+
+            body {
+
+                margin:0;
+                border:0;
+
+            }
 		
 			a	{
 			
 				color: black;
 				text-decoration: none;
 			
+            }
+
+            li {
+
+                font-size: 135%;
+
             }
 
             a:hover {
@@ -25,7 +38,7 @@
 
             }
 		
-			.first-section {
+			.videolinks {
 			
 				background-color:#4285F4;
 				float:left;
@@ -33,17 +46,34 @@
 				position:relative;
 				opacity:0.8;
 				border-radius: 10px;
+                margin:8px;
 				font-family: 'Architects Daughter', cursive, KaiTi;
-				font-size: 120%;
+                font-size: 120%;
+                
 			
 			}
 		
-			.second-section {
+			.nicecar {
 			
 				float:right;
-		
+                margin:8px;
 			
 			}
+
+            .small-games {
+
+				background-color:#F7DC9E;
+				float:left;
+				width:150px;
+				position:relative;
+				opacity:0.8;
+				border-radius: 10px;
+                margin:8px;
+				font-family: 'Architects Daughter', cursive, KaiTi;
+                font-size: 120%;
+
+
+            }
 		
 		
 		</style>
@@ -57,50 +87,62 @@
 
 	<body>
 	
-		<div class="first-section">
+		<div class="videolinks">
 		
 			<h1>video</h1>
 			
 				<ul>
 					
 					<li>game</li>
-                        <a href="http://space.bilibili.com/1565155/#!/">主播真会玩</a><br>
-						<a href="http://space.bilibili.com/10462362/#!/">逗鱼时刻</a><br>
-						<a href="http://space.bilibili.com/20990353/#!/">努巴尼欢乐秀</a><br>
+                        <a href="http://space.bilibili.com/1565155/#!/"target='_blank'>主播真会玩</a><br>
+						<a href="http://space.bilibili.com/10462362/#!/"target='_blank'>逗鱼时刻</a><br>
+						<a href="http://space.bilibili.com/20990353/#!/"target='_blank'>努巴尼欢乐秀</a><br>
 					
 
 					<li>animation</li>
-						<a href="http://bangumi.bilibili.com/anime/5978/">傅人传</a><br>
-						<a href="http://www.dnvod.tv/Movie/detail.aspx?id=2aGLezX2DZk%3d">进击的巨人</a><br>
+						<a
+                            href="http://bangumi.bilibili.com/anime/5978/" target='_blank'>傅人传</a><br>
+						<a href="http://www.dnvod.tv/Movie/detail.aspx?id=2aGLezX2DZk%3d" target='_blank'>进击的巨人</a><br>
 						
 					<li>funny</li>
-						<a href="http://www.bilibili.com/video/life.html">好玩</a><br>
+						<a href="http://www.bilibili.com/video/life.html" target='_blank'>好玩</a><br>
 
-						
+
 					<li>quick movie review</li>
-						<a href="http://space.bilibili.com/927587/#!/">木鱼</a><br>
+						<a href="http://space.bilibili.com/927587/#!/" target='_blank'>木鱼</a><br>
 						
 					<li>bao zou da shi jian</li>
 					
-						<a href="http://space.bilibili.com/883968/#!/">暴走大事件</a><br>
+						<a href="http://space.bilibili.com/883968/#!/" target='_blank'>暴走大事件</a><br>
 						
 					<li>series</li>
-						<a href="http://www.dnvod.tv/Movie/detail.aspx?id=BhdIRzJebHQ%3d">人民的名义</a><br>
+						<a
+                            href="http://www.dnvod.tv/Movie/detail.aspx?id=BhdIRzJebHQ%3d" target='_blank'>人民的名义</a><br>
 						
 					<li>guita</li>
 					
 					<li>asmr</li>
-						<a href="https://www.youtube.com/watch?v=q2PgiV9_K0s">轩子巨二兔1</a><br>
-						<a href="https://www.youtube.com/watch?v=gQJ6fqQWgns">轩子巨二兔2</a><br>
+						<a href="https://www.youtube.com/watch?v=q2PgiV9_K0s"target='_blank'>轩子巨二兔1</a><br>
+						<a href="https://www.youtube.com/watch?v=gQJ6fqQWgns"target='_blank'>轩子巨二兔2</a><br>
 				</ul>
 			
 		</div>
 		
-		<div class="second-section">
+		<div class="nicecar">
 		
 			<iframe width="560" height="315" src="https://www.youtube.com/embed/0QjF6A3Zwkk" frameborder="0" allowfullscreen></iframe><br>
 		
 		</div>
+
+        <div class="small-games">
+
+            <h1>Games</h1>
+
+                <a href="ratsgame.html">Rats Game</a>
+
+
+
+        </div>
 	
 	</body>
 
